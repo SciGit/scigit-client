76ee6ed408ef1a5c53af377d1c256f7e299a43a7
hanson.wang <hanson.wng@gmail.com>
2012-10-07 22:38:28 -0400

    Missed some files

---
+++ SciGit-Client/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using log4net;
+
+namespace SciGit_Client
+{
+  class Logger
+  {
+    private static ILog log;
+    static Logger() {
+      var pattern = "%date %level - %message %exception";
+      var appender = new log4net.Appender.RollingFileAppender {
+        Layout = new log4net.Layout.PatternLayout(pattern),
+        File = "error.log",
+        AppendToFile = true,
+        RollingStyle = log4net.Appender.RollingFileAppender.RollingMode.Composite,
+        MaxSizeRollBackups = 1,
+        MaximumFileSize = "10MB",
+        StaticLogFileName = true
+      };
+      appender.ActivateOptions();
+      log4net.Config.BasicConfigurator.Configure(appender);
+      log = LogManager.GetLogger("SciGit");
+    }
+
+    public static void LogException(Exception e) {
+      if (log.IsErrorEnabled) {
+        while (e.InnerException != null) e = e.InnerException;
+        log.Error("Exception:", e);
+      }
+    }
+
+    public static void LogMessage(string message) {
+      if (log.IsErrorEnabled) {
+        log.Error(message);
+      }
+    }
+  }
+}
---
+++ SciGit-Client/ShellCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SciGit_Client
+{
+  class ShellCommandHandler
+  {
+    public delegate void CommandHandler(string s, string t);
+    private NamedPipeServerStream pipeServer;
+    private Thread pipeThread;
+
+    public ShellCommandHandler(CommandHandler cmdHandler) {
+      pipeServer = new NamedPipeServerStream("sciGitPipe", PipeDirection.In, 1, PipeTransmissionMode.Byte,
+                                             PipeOptions.Asynchronous); 
+      pipeThread = new Thread(() => {
+        while (true) {
+          try {
+            var ar = pipeServer.BeginWaitForConnection(null, null);
+            pipeServer.EndWaitForConnection(ar);
+            var ss = new StreamString(pipeServer);
+            string verb = ss.ReadString();
+            string filename = ss.ReadString();
+            cmdHandler(verb, filename);
+            pipeServer.Disconnect();
+          } catch (ObjectDisposedException) {
+            break;
+          } catch (IOException) {
+            break;
+          } catch (Exception e) {
+            Logger.LogException(e);
+          }
+        }
+      });
+    }
+
+    public void Start() {
+      pipeThread.Start();
+    }
+
+    public void Stop() {
+      pipeServer.Dispose();
+      pipeThread.Join();
+    }
+  }
+}
