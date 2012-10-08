438d4cbdf5b0aadfc6e6bac0b646d607c0a2cb84
hanson.wang <hanson.wng@gmail.com>
2012-10-08 02:02:39 -0400

    Starting settings manager

---
+++ SciGit-Client/SettingsForm.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SciGit_Client
+{
+  /// <summary>
+  /// Interaction logic for SettingsForm.xaml
+  /// </summary>
+  public partial class SettingsForm : Window
+  {
+    public SettingsForm() {
+      InitializeComponent();
+    }
+
+    private void ClickChooseFolder(object sender, EventArgs e) {
+    }
+
+    private void ClickManageProjects(object sender, EventArgs e) {
+      Process.Start("http://" + RestClient.ServerHost + "/projects");
+    }
+
+    private void ClickManageAccount(object sender, EventArgs e) {
+      Process.Start("http://" + RestClient.ServerHost + "/users/profile");
+    }
+
+    private void ClickOK(object sender, EventArgs e) {
+
+    }
+
+    private void ClickCancel(object sender, EventArgs e) {
+      Close();
+    }
+  }
+}
